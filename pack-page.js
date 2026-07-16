20962e7fdc48a947dbbdd4eb96d3d728826edb73
justdonnyScenepacks <donnavanlehman15@gmail.com>
2026-07-16 18:53:27 +0200

    Create pack-page.js

---
+++ pack-page.js
@@ -0,0 +1,66 @@
+const params = new URLSearchParams(window.location.search);
+
+const club = params.get("club");
+
+
+document.getElementById("club-title").innerHTML =
+club + " SCENEPACKS";
+
+
+const container = document.getElementById("pack-container");
+
+
+const packs = scenepacks.filter(pack =>
+    pack.name === club
+);
+
+
+packs.forEach(pack => {
+
+
+const card = document.createElement("div");
+
+card.className="pack";
+
+
+card.innerHTML=`
+
+<img src="${pack.image}">
+
+<div class="pack-name">
+${pack.name}
+</div>
+
+`;
+
+
+container.appendChild(card);
+
+
+});
+
+
+
+/* SEARCH */
+
+document.getElementById("search")
+.addEventListener("input", e=>{
+
+
+let value=e.target.value.toLowerCase();
+
+
+document.querySelectorAll(".pack")
+.forEach(pack=>{
+
+
+pack.style.display =
+pack.innerText.toLowerCase().includes(value)
+?"block"
+:"none";
+
+
+});
+
+
+});
